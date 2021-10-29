56bbe3d803d9e73749ef665491575f9e5988cf77
ashfaqEaglesoft <yahooashfaq@gmail.com>
2021-10-29 14:52:57 -0700

    first commit

---
+++ Activity Slot v1.0.py
@@ -0,0 +1,33 @@
+import pyttsx3
+while True:
+    opt = input("Enter A to write your acitivity \nEnter B to read your Activity")
+
+    def writeActivity():
+        Act = input("Kindly Enter Your Acitivity Here\t")
+        t = input("Enter Exact Time Of Acitivity\t")
+        d = input("Enter Today Date\t")
+        file = open("C:/Users/Public/myactivityslot.txt" ,"a+")
+        file.write("Acitivity\t" + Act + "\tTime\t" + t + "\tDate\t" + d +"\n")
+        file.close()
+
+    def readActivity():
+        file = open("C:/Users/Public/myactivityslot.txt", "r")
+        c = input("Enter The Keyword That You Want to See")
+        Detective = 0
+        for mydat in file:
+            if c in mydat:
+                print(mydat)
+                engine = pyttsx3.init()
+                engine.say(mydat)
+                engine.runAndWait()
+                Detective = 1
+        if Detective == 0:
+            print("Data Not Found")
+        file.close()
+
+    if opt == "A" or opt=="a":
+        writeActivity()
+    elif opt == "B" or opt=="b":
+        readActivity()
+    else:
+        print("invlaid input! try agian with[A/B]\n\n")
