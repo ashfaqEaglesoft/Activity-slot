404354682b90c20e3febc97b584ceb82cce949be
ashfaqEaglesoft <yahooashfaq@gmail.com>
2021-10-31 06:36:28 -0700

    Activity slot v1.1

--- Activity Slot v1.0.py
+++ Activity Slot v1.0.py
@@ -1,11 +1,15 @@
+from datetime import datetime,date
 import pyttsx3
 while True:
     opt = input("Enter A to write your acitivity \nEnter B to read your Activity")
 
     def writeActivity():
         Act = input("Kindly Enter Your Acitivity Here\t")
-        t = input("Enter Exact Time Of Acitivity\t")
-        d = input("Enter Today Date\t")
+        now=datetime.now()
+        t = now.strftime("%H:%M:%S")
+        today=date.today()
+        todaydate = today.strftime("%B %d, %Y")
+        d = todaydate
         file = open("C:/Users/Public/myactivityslot.txt" ,"a+")
         file.write("Acitivity\t" + Act + "\tTime\t" + t + "\tDate\t" + d +"\n")
         file.close()
